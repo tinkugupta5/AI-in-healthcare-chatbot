49832a82b1660f6dd6b675fdb8c54de38ab4b908
tinkugupta5 <tinku.gupta4@gmail.com>
2026-08-20 00:36:51 +0530

    Add compiled bytecode for create_memory_for_llm module
    This commit introduces the compiled bytecode for the create_memory_for_llm.py module, enabling faster loading and execution. The bytecode is generated for Python 3.14 and is stored in the __pycache__ directory.

---
+++ create_memory_for_llm.py
@@ -0,0 +1,18 @@
+# step 1 Load raw PDF
+# step 2 Create Chunks
+# step 3 create vector embeddings
+# step 4 store embeddings
+
+from langchain_community.document_loaders import PyPDFLoader,DirectoryLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+DATA_PATH = "data/"
+def load_pdf_files(data):
+    loader = DirectoryLoader(
+        data,
+        glob="*.pdf",
+        loader_cls=PyPDFLoader
+    )
+
+    documents = loader.load()
+    return documents
