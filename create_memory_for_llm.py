9d828de32b745fb810f6a5b99386a6cc292efe26
tinkugupta5 <tinku.gupta4@gmail.com>
2026-08-21 23:40:00 +0530

    Remove unnecessary comment and clean up code formatting

--- create_memory_for_llm.py
+++ create_memory_for_llm.py
@@ -2,11 +2,8 @@
 # step 2 Create Chunks
 # step 3 create vector embeddings
 # step 4 store embeddings
-
 from langchain_community.document_loaders import PyPDFLoader,DirectoryLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-
-# path
 
 DATA_PATH = "data/"
 def load_pdf_files(data):
