86b7bd488bab1f0eb8c59e311b7cec8174627422
tinkugupta5 <tinku.gupta4@gmail.com>
2026-08-26 00:23:28 +0530

    Update index.md with environment activation instructions and project run details

--- create_memory_for_llm.py
+++ create_memory_for_llm.py
@@ -1,8 +1,6 @@
 # step 1 Load raw PDF
-# step 2 Create Chunks
-# step 3 create vector embeddings
-# step 4 store embeddings
-from langchain_community.document_loaders import PyPDFLoader,DirectoryLoader
+
+from langchain_community.document_loaders import DirectoryLoader, PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 DATA_PATH = "data/"
@@ -13,6 +11,28 @@ def load_pdf_files(data):
         glob="*.pdf",
         loader_cls=PyPDFLoader
     )
-    
+
     documents = loader.load()
+    print("Length of PDF pages : ", len(documents))
     return documents
+
+
+def create_chunks(extracted_data):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500,
+        chunk_overlap=50,
+    )
+    return text_splitter.split_documents(extracted_data)
+
+
+if __name__ == "__main__":
+    documents = load_pdf_files(DATA_PATH)
+    text_chunks = create_chunks(documents)
+    print("Length of Text Chunks: ", len(text_chunks))
+
+    # step 3 create vector embeddings
+    
+    
+    
+    
+    # step 4 store embeddings
--- index.md
+++ index.md
@@ -5,3 +5,34 @@ python -m pipenv shell
 
 video link:
 https://youtu.be/OP0FYjF-37c?list=PL_pAv_JZgZkFV1Ct2qPDeAkddyz2ahBUX&t=1183
+
+
+======================== .venv ==================================================
+
+.\.venv\Scripts\Activate.ps1
+You should see:
+(.venv) PS C:\Users\Tinku Gupta\OneDrive\Desktop\medical_chatboat>
+
+Then verify it:
+python -c "import sys; print(sys.executable)"
+
+
+============================== finish =====================================
+
+==========================How to run the project ==========================
+pipenv run python .\create_memory_for_llm.py
+
+====================end ================
+
+Run this in the current window:
+.\.venv-1\Scripts\activate.bat
+
+Your prompt should become:
+(.venv-1) C:\Users\Tinku Gupta\OneDrive\Desktop\medical_chatboat>
+
+Alternatively, exit the current subshell:
+exit
+
+Then, in PowerShell, run:
+.\.venv-1\Scripts\Activate.ps1
+
